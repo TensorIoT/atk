d8410c7835597d163dd96e4d35ceb2c7ba59feb2
surthakur <87124706+surthakur@users.noreply.github.com>
2021-07-14 20:49:42 +0530

    Auth Fix
    Auth Fix

--- Frontend/src/AppWithAuth.jsx
+++ Frontend/src/AppWithAuth.jsx
@@ -1,38 +1,90 @@
-import React, { useState, Suspense } from "react";
-import { AmplifyAuthenticator } from "@aws-amplify/ui-react";
-import { AuthState } from "@aws-amplify/ui-components";
+import React, { useState, Suspense } from 'react';
+import {
+	AmplifyAuthenticator,
+	AmplifyRequireNewPassword,
+} from '@aws-amplify/ui-react';
+import {
+	AuthState,
+	onAuthUIStateChange,
+	UI_AUTH_CHANNEL,
+	AUTH_STATE_CHANGE_EVENT,
+} from '@aws-amplify/ui-components';
+import { Hub } from 'aws-amplify';
+import { makeStyles } from '@material-ui/core/styles';
 
-import CustomSignIn from "./CustomSignIn";
+import CustomSignIn from './CustomSignIn';
 
-const App = React.lazy(() => import("App"));
+const App = React.lazy(() => import('App'));
 
 const AppWithAuth = () => {
-  const [authState, setAuthState] = useState();
+	const classes = useStyles();
+	const [authState, setAuthState] = useState();
+	const [authData, setAuthData] = React.useState(null);
 
-  const signedIn = authState === AuthState.SignedIn;
+	const signedIn = authState === AuthState.SignedIn;
 
-  return (
-    <Suspense fallback={<div />}>
-      <AmplifyAuthenticator
-        handleAuthStateChange={(authState) => setAuthState(authState)}
-      >
-        {signedIn ? (
-          <App />
-        ) : (
-          <div
-            slot="sign-in"
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              marginTop: "15rem",
-            }}
-          >
-            <CustomSignIn slot="sign-in" />
-          </div>
-        )}
-      </AmplifyAuthenticator>
-    </Suspense>
-  );
+	const handleAuthStateChange = async (nextAuthState, authData) => {
+		Hub.dispatch(UI_AUTH_CHANNEL, {
+			event: AUTH_STATE_CHANGE_EVENT,
+			message: nextAuthState,
+			data: authData ? authData : null,
+		});
+	};
+
+	React.useEffect(() => {
+		return onAuthUIStateChange((nextAuthState, authData) => {
+			setAuthState(nextAuthState);
+			setAuthData(authData);
+		});
+	}, []);
+
+	React.useEffect(() => {
+		return onAuthUIStateChange((nextAuthState, authData) => {
+			setAuthState(nextAuthState);
+			setAuthData(authData);
+		});
+	}, []);
+
+	return (
+		<Suspense fallback={<div />}>
+			<AmplifyAuthenticator
+				handleAuthStateChange={(authState) => setAuthState(authState)}
+			>
+				{signedIn ? (
+					<App />
+				) : (
+					<>
+						<div
+							slot="sign-in"
+							style={{
+								display: 'flex',
+								justifyContent: 'center',
+								marginTop: '15rem',
+							}}
+						>
+							<CustomSignIn signIn={handleAuthStateChange} />
+						</div>
+						<div slot="require-new-password">
+							<AmplifyRequireNewPassword
+								user={authData}
+								handleAuthStateChange={handleAuthStateChange}
+								slot="require-new-password"
+								className={classes.amplifyDefaultContainer}
+							/>
+						</div>
+					</>
+				)}
+			</AmplifyAuthenticator>
+		</Suspense>
+	);
 };
+
+const useStyles = makeStyles((theme) => ({
+	amplifyDefaultContainer: {
+		display: 'flex',
+		justifyContent: 'center',
+		margin: '1rem auto 0 auto',
+	},
+}));
 
 export default AppWithAuth;
--- Frontend/src/CustomSignIn.jsx
+++ Frontend/src/CustomSignIn.jsx
@@ -1,133 +1,141 @@
-import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import TextField from "@material-ui/core/TextField";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import { Auth } from "aws-amplify";
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { Auth } from 'aws-amplify';
+import { AuthState, ChallengeName } from '@aws-amplify/ui-components';
 
-const CustomSignin = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [openDialog, setOpenDialog] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
+const CustomSignin = ({ signIn }) => {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [openDialog, setOpenDialog] = useState(false);
+	const [errorMessage, setErrorMessage] = useState('');
 
-  const signIn = async (e) => {
-    e.preventDefault();
-    try {
-      const user = await Auth.signIn(username, password);
-      if (user) {
-        setUsername("");
-        setPassword("");
-        window.location.reload(false);
-      }
-    } catch (error) {
-      console.log("error signing in", error);
-      setErrorMessage(error.message);
-      handleDialogOpen();
-    }
-  };
+	const handleSignIn = async (e) => {
+		e.preventDefault();
+		try {
+			const user = await Auth.signIn(username, password);
+			if (user) {
+				setUsername('');
+				setPassword('');
+				if (user.challengeName === ChallengeName.NewPasswordRequired) {
+					return signIn(AuthState.ResetPassword, user);
+				}
+				const verifyData = await Auth.verifiedContact(user);
+				if (verifyData?.verified?.email) {
+					console.log('user is verified, signing them in');
+					signIn(AuthState.SignedIn, user);
+				}
+			}
+		} catch (error) {
+			console.log('error signing in', error);
+			setErrorMessage(error.message);
+			handleDialogOpen();
+		}
+	};
 
-  const handleDialogOpen = () => {
-    setOpenDialog(true);
-  };
+	const handleDialogOpen = () => {
+		setOpenDialog(true);
+	};
 
-  const handleDialogClose = () => {
-    setOpenDialog(false);
-  };
+	const handleDialogClose = () => {
+		setOpenDialog(false);
+	};
 
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <Paper elevation={5} className={classes.container}>
-      <Dialog open={openDialog} onClose={handleDialogClose}>
-        <DialogTitle className={classes.dialogTitle}>Opps...</DialogTitle>
-        <p className={classes.errorMessage}>{errorMessage}</p>
-        <DialogActions>
-          <button
-            className={classes.dialogBtn}
-            onClick={() => handleDialogClose()}
-          >
-            Close
-          </button>
-        </DialogActions>
-      </Dialog>
-      <h3>Sign in to your account</h3>
-      <form
-        style={{
-          display: "flex",
-          flexDirection: "column",
-        }}
-        onSubmit={(e) => signIn(e)}
-      >
-        <TextField
-          variant="outlined"
-          label="username"
-          value={username}
-          className={classes.input}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <TextField
-          variant="outlined"
-          label="password"
-          type="password"
-          value={password}
-          className={classes.input}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit" className={classes.signInBtn}>
-          Sign In
-        </button>
-      </form>
-    </Paper>
-  );
+	return (
+		<Paper elevation={5} className={classes.container}>
+			<Dialog open={openDialog} onClose={handleDialogClose}>
+				<DialogTitle className={classes.dialogTitle}>Opps...</DialogTitle>
+				<p className={classes.errorMessage}>{errorMessage}</p>
+				<DialogActions>
+					<button
+						className={classes.dialogBtn}
+						onClick={() => handleDialogClose()}
+					>
+						Close
+					</button>
+				</DialogActions>
+			</Dialog>
+			<h3>Sign in to your account</h3>
+			<form
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+				}}
+				onSubmit={(e) => handleSignIn(e)}
+			>
+				<TextField
+					variant="outlined"
+					label="username"
+					value={username}
+					className={classes.input}
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+				<TextField
+					variant="outlined"
+					label="password"
+					type="password"
+					value={password}
+					className={classes.input}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<button type="submit" className={classes.signInBtn}>
+					Sign In
+				</button>
+			</form>
+		</Paper>
+	);
 };
 
 export default CustomSignin;
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    padding: "1rem 2rem 3rem 2rem",
-    width: "30rem",
-  },
-  input: {
-    marginBottom: "2rem",
-  },
-  signInBtn: {
-    width: "30%",
-    height: "3rem",
-    marginLeft: "auto",
-    backgroundColor: "steelblue",
-    color: "white",
-    border: "none",
-    borderRadius: "5px",
-    fontSize: "0.9rem",
-    fontWeight: "600",
-    cursor: "pointer",
-    "&:hover": {
-      opacity: "0.75",
-    },
-  },
-  dialogTitle: {
-    paddingLeft: "3rem",
-  },
-  errorMessage: {
-    margin: "0 3rem 1rem 3rem",
-  },
-  dialogBtn: {
-    backgroundColor: "steelblue",
-    color: "white",
-    height: "2.1rem",
-    width: "4rem",
-    border: "none",
-    borderRadius: "5px",
-    fontSize: "0.9rem",
-    fontWeight: "600",
-    cursor: "pointer",
-    margin: "0 2rem 1rem 0",
-    "&:hover": {
-      opacity: "0.75",
-    },
-  },
+	container: {
+		padding: '1rem 2rem 3rem 2rem',
+		width: '30rem',
+	},
+	input: {
+		marginBottom: '2rem',
+	},
+	signInBtn: {
+		width: '30%',
+		height: '3rem',
+		marginLeft: 'auto',
+		backgroundColor: 'steelblue',
+		color: 'white',
+		border: 'none',
+		borderRadius: '5px',
+		fontSize: '0.9rem',
+		fontWeight: '600',
+		cursor: 'pointer',
+		'&:hover': {
+			opacity: '0.75',
+		},
+	},
+	dialogTitle: {
+		paddingLeft: '3rem',
+	},
+	errorMessage: {
+		margin: '0 3rem 1rem 3rem',
+	},
+	dialogBtn: {
+		backgroundColor: 'steelblue',
+		color: 'white',
+		height: '2.1rem',
+		width: '4rem',
+		border: 'none',
+		borderRadius: '5px',
+		fontSize: '0.9rem',
+		fontWeight: '600',
+		cursor: 'pointer',
+		margin: '0 2rem 1rem 0',
+		'&:hover': {
+			opacity: '0.75',
+		},
+	},
 }));
